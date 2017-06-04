c1c2cbed8217f955b4f6b80c673ca990f9863d69
Michelle Yang <zomgitsmichelle@gmail.com>
2017-06-04 13:32:33 -0700

    Adding config file.

---
+++ config.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBtxRDOw6HGnwrQwlXpb7YUXUSVVAdhHKA",
+  authDomain: "hackxx-a1d10.firebaseapp.com",
+  databaseURL: "https://hackxx-a1d10.firebaseio.com",
+  storageBucket: "hackxx-a1d10.appspot.com",
+};
+firebase.initializeApp(config);
